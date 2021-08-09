241766f1622b68666c31ac37cb0fa34ee8c01466
deepak123bharat <dsrdeepak8@gmail.com>
2021-08-09 18:50:47 +0530

    Adding CI

--- spec/rockstar_spec.lua
+++ spec/rockstar_spec.lua
@@ -1,13 +1,18 @@
 local tar = require("rocks.tar")
+local fs = require("rocks.fs")
 
 describe("Luarocks tar test #unit", function()
 
+   setup(function ()
+      fs.init({"linux","unix"})
+   end)
     describe("tar.untar", function()
        it("unpacking .tar archives", function()
           local t, result
-          t, result = tar.untar("spec/file.tar", "dest")
-          assert.falsy(result)
+          t, result = tar.untar("spec/luarocks-3.6.0.tar.gz", "dest")
+          print(result)
           assert.truthy(t)
+          assert.falsy(result)     
        end)
     end)
 end)
